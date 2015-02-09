949f8e3ced11d3919b0515b37d6d7496a4979b87
spvn <wkan2003@gmail.com>
2015-02-09 17:01:03 +0800

    First commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn(){
+
+	}
+}
---
+++ Assets/Script/ScriptEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void shootBullet(){
+
+	}
+
+	void OnClick(){
+		shootBullet ();
+	}
+}
