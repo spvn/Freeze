c832d9a42713bbfd268395c3de382d0a3296fbbb
spvn <wkan2003@gmail.com>
2015-03-03 00:56:52 +0800

    Some basic (very broken) movement

--- Assets/Script/scriptMovement.cs
+++ Assets/Script/scriptMovement.cs
@@ -5,11 +5,15 @@ public class scriptMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		iTween.MoveTo(gameObject, iTween.Hash ("path", iTweenPath.GetPath("path1"), "time", 10));
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		float horizontal = Input.GetAxis("Horizontal")/2;
+		
+		Vector3 direction = new Vector3(horizontal, 0, 0.1f);
+		transform.position += direction;
 	}
 }
