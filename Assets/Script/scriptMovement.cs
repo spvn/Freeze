9107065453e4bdc0fb388b492ff82735bb80eb37
spvn <wkan2003@gmail.com>
2015-03-06 02:57:07 +0800

    Movement is 90% working
    Working on better raycasting now

--- Assets/Script/scriptMovement.cs
+++ Assets/Script/scriptMovement.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class scriptMovement : MonoBehaviour {
 	
+	public float playerSpeed = 1.0f;
+	
+	
+	private CharacterController controller;
+	private Vector3 direction;
+	public bool hasCollisionInFront;
+	public float collisionDist = 0.5f;
+	
 	// Use this for initialization
 	void Start () {
-
+		controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -13,11 +21,24 @@ public class scriptMovement : MonoBehaviour {
 	
 		float horizontal = Input.GetAxis("Horizontal")/2;
 		
-		Vector3 direction = new Vector3(horizontal, 0, 0.0f);
-		transform.position += direction;
+		direction = new Vector3(horizontal, 0, 0.0f);
+		//transform.position += direction;
+		controller.SimpleMove(direction * playerSpeed);
+		
+		
+		RaycastHit hit;
+		Ray checkCollisionRay = new Ray(transform.position, transform.forward);
+		
+		if (Physics.Raycast(checkCollisionRay, out hit, collisionDist)) {
+			hasCollisionInFront = true;
+		}
+		else {
+			hasCollisionInFront = false;
+		}
+		
 	}
 
-	void OnTriggerEnter( Collider col )
+	/*void OnTriggerEnter( Collider col )
 	{
 		Debug.Log ("TRIGGERED");
 		pausePath ();
@@ -37,7 +58,7 @@ public class scriptMovement : MonoBehaviour {
 	void resumePath()
 	{
 		iTween.Resume ();
-	}
+	}*/
 	
 	void OnCollisionEnter(Collision col) {
 		Debug.Log (gameObject.name + " has collided with " + col.gameObject.name);
---
+++ Assets/scriptPlayerHolder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptPlayerHolder : MonoBehaviour {
+	
+	public Transform[] pathNodes;
+	public float moveSpeed = 1.0f;
+	
+	
+	private int currNode = 0;
+	private Transform player;
+	
+	
+	// Use this for initialization
+	void Start () {
+		player = transform.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if (transform.position == pathNodes[currNode].position) {
+			currNode++;
+		}
+		
+		if (!player.GetComponent<scriptMovement>().hasCollisionInFront) {
+			transform.position = Vector3.MoveTowards (transform.position, pathNodes[currNode].position, moveSpeed * Time.deltaTime);
+		}
+	}
+}
