1a8552d083eaea2722bb1cdfbd352a140d9c100e
spvn <wkan2003@gmail.com>
2015-03-13 19:09:37 +0800

    WIP fixing bullet collision

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BulletScript : MonoBehaviour {
 	Vector3 targetPoint;
-	public float duration = 0.0000001f;
+	public float duration = 0.0001f;
 	public scriptMovement playerMovement;
 
 	Vector3 bulletDirection = Vector3.zero;
@@ -22,13 +22,15 @@ public class BulletScript : MonoBehaviour {
 	void Update () {
 
 		if ( bulletDirection != Vector3.zero && !playerMovement.isFrozen) {
-			this.transform.localPosition += bulletDirection * Time.deltaTime * duration;
+			this.transform.localPosition += Vector3.Normalize(bulletDirection) * Time.deltaTime * duration;
 		}
 	}
 
 	void OnCollisionEnter( Collision col )
 	{
-		Debug.Log ("Deleting bullet " + col.gameObject.name + " at " + this.transform.localPosition.ToString());
-		Destroy(gameObject);
+		if (col.gameObject.name != "PlayerHolder") {
+			Debug.Log ("Deleting bullet " + col.gameObject.name + " at " + this.transform.localPosition.ToString());
+			Destroy(gameObject);
+		}
 	}
 }
