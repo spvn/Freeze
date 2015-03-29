be4a884477c1130d6827565b171ca624ab5e414d
jinglah <Jing@Jings-MacBook-Air-2.local>
2015-03-29 22:47:38 +0800

    Added threshold that enemy can rotate to face player

--- Assets/Script/ScriptEnemy.cs
+++ Assets/Script/ScriptEnemy.cs
@@ -10,20 +10,36 @@ public class ScriptEnemy : MonoBehaviour {
 
 	float timer = 0.0f;
 	float intervalShootTime = 1.0f;
+	Quaternion initialAngle;
+
 	// Use this for initialization
 	void Start () {
-
+		initialAngle = this.transform.localRotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (isFacingPlayer() && !player.GetComponent<scriptMovement>().isFrozen) {
+		if (isFacingPlayer() && !player.GetComponent<scriptMovement>().isFrozen && withinRotationRange()) {
+
+			transform.LookAt(player.transform);
+		
 			timer += Time.deltaTime;
 			if (timer > intervalShootTime) {
 				timer = 0.0f;
 				shootBullet ();
 			}
 		}
+	}
+
+	bool withinRotationRange()
+	{
+		float rotationAngle = Mathf.Abs ((this.transform.rotation.y) - initialAngle.y) * Mathf.Rad2Deg;
+		Debug.Log (this.transform.rotation.y * Mathf.Rad2Deg + " " + initialAngle.y * Mathf.Rad2Deg);
+		if (rotationAngle <= 30.0f && rotationAngle >= 0.0f) {
+			return true;
+		}
+		Debug.Log ("Not turning " + rotationAngle);
+		return false;
 	}
 
 	bool isFacingPlayer()
