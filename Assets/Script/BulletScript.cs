e8f452a6dcb9c5bc02e8148eb702d4b603ff551b
jinglah <Jing@Jings-MacBook-Air-2.local>
2015-03-31 22:18:43 +0800

    Line renderer now changes color

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class BulletScript : MonoBehaviour {
 	Vector3 targetPoint;
+	Vector3 firstPoint;
 	public float duration;
 	public scriptMovement playerMovement;
 	public LineRenderer bulletLine;
 
 	Vector3 bulletDirection = Vector3.zero;
+	float bulletLineLength;
+	RaycastHit objHit;
 
 	// Use this for initialization
 	void Start () {
@@ -23,17 +26,30 @@ public class BulletScript : MonoBehaviour {
 
 		Vector3 lineFirstPoint = this.transform.position + (bulletDirection);
 		this.transform.localPosition = lineFirstPoint;
+		firstPoint = this.transform.localPosition;
 		bulletLine.SetPosition (0, lineFirstPoint);
 		bulletLine.SetPosition (1, targetPoint);
 		bulletLine.SetPosition (2, targetPoint + (bulletDirection * 10));
+		bulletLineLength = Vector3.Distance (lineFirstPoint, (targetPoint + (bulletDirection * 100))); 
 	}
 	// Update is called once per frame
 	void Update () {
 
+		checkHitPlayer ();
 		if ( !playerMovement.isGameOver && bulletDirection != Vector3.zero && !playerMovement.isFrozen) {
 			this.transform.localPosition += bulletDirection * duration * Time.deltaTime;
 		}
 
+	}
+
+	void checkHitPlayer()
+	{
+		if (Physics.Raycast (firstPoint, bulletDirection, out objHit, Mathf.Infinity, (1<<0)) && objHit.collider.gameObject.name == "Player") {
+			bulletLine.SetColors (Color.red, Color.red);
+		} else {
+			bulletLine.SetColors(Color.green, Color.green);
+		}
+	
 	}
 
 	void OnCollisionEnter( Collision col )
