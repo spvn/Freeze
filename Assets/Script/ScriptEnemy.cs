28da8276fe66c7c6e3484febf6f79529c33368b5
jinglah <Jing@Jings-MacBook-Air-2.local>
2015-04-10 19:59:38 +0800

    Animation for enemy added

--- Assets/Script/ScriptEnemy.cs
+++ Assets/Script/ScriptEnemy.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 
 public class ScriptEnemy : MonoBehaviour {
-	public GameObject playerHolder;
-
 	public GameObject player;
 	bool shooting = false;
 	public GameObject bulletPrefab;
+	public Animator enemyAnimator;
 
 	float timer = 0.0f;
-	public float intervalShootTime = 1.0f;
+	public float intervalShootTime;
 	public bool isHostile = true;
 	Quaternion initialAngle;
+	Vector3 bulletOffset = new Vector3(0, 2.5f, 0);
+	Vector3 playerOffset = new Vector3(0, 0.3f, 0);
+	Vector3 randomOffset;
+
+	bool isAiming = false;
 
 	// Use this for initialization
 	void Start () {
@@ -21,8 +25,13 @@ public class ScriptEnemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (isHostile){
+			Debug.Log(gameObject.name + " " + isFacingPlayer() + " " + player.GetComponent<scriptMovement>().isFrozen + withinRotationRange());
 			if (isFacingPlayer() && !player.GetComponent<scriptMovement>().isFrozen && withinRotationRange()) {
-	
+				if(!isAiming)
+				{
+					isAiming = true;
+					enemyAnimator.SetBool("Aim", true);
+				}
 				transform.LookAt(player.transform);
 			
 				timer += Time.deltaTime;
@@ -49,10 +58,10 @@ public class ScriptEnemy : MonoBehaviour {
 	{
 		Vector3 playerPos = player.transform.position;
 		float distance = Vector3.Distance (this.transform.position, playerPos) - 1;
-		Vector3 direction = playerPos - this.transform.position;
+		Vector3 direction = playerPos - (this.transform.position + bulletOffset);
 	
-		//Debug.DrawRay (this.transform.position, direction);
-		if (!Physics.Raycast(this.transform.position, direction, distance, (1<<9 | 1 <<0))) {
+		Debug.DrawRay (this.transform.position + bulletOffset, direction);
+		if (!Physics.Raycast(this.transform.position + bulletOffset, direction, distance, (1<<9 | 1 <<0))) {
 //			Debug.Log("Enemy is facing player");
 			return true;
 		}
@@ -68,12 +77,13 @@ public class ScriptEnemy : MonoBehaviour {
 		}
 
 		shooting = true;
+		enemyAnimator.Play ("Fire 1Pistol");
 
 		GameObject bullet = (GameObject)Instantiate (bulletPrefab);
 
-		bullet.transform.position = this.transform.position + new Vector3(0,0.3f,0);
+		bullet.transform.position = this.transform.position + bulletOffset;
 
-		bulletTargetPoint = player.transform.position;
+		bulletTargetPoint = player.transform.position + playerOffset;
 		bullet.GetComponent<BulletScript> ().setBulletDirection (bulletTargetPoint);
 		bullet.GetComponent<BulletScript> ().playerMovement = player.GetComponent<scriptMovement>();
 
