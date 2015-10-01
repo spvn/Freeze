14ac0500473a085f6edafaf99e12d494bdf9f1c0
pigoblock <kowanling@hotmail.com>
2015-10-01 17:03:40 +0800

    Chase on left/right of player

--- Assets/Script/Enemy/TurretMoving.cs
+++ Assets/Script/Enemy/TurretMoving.cs
@@ -1,30 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TurretMoving : MonoBehaviour {
+public enum AppearSide{
+	APPEAR_LEFT,
+	APPEAR_RIGHT
+}
 
-	// Stopping distance must be > 3
+public class TurretMoving : MonoBehaviour {
 	public float speed;
+	public AppearSide chaseSide;
 
 	private GameManager gameManager;
 	private GameObject player;
 	private NavMeshAgent enemy;
-	private Vector3 side;
+	private Vector3 rightSide;
+	private Vector3 leftSide;
 
 	// Use this for initialization
 	void Start () {
 		gameManager = GameObject.Find ("Game Manager").GetComponent<GameManager>();
 		player = GameObject.Find ("OVRCameraRig");
 		enemy = GetComponent<NavMeshAgent> ();
-		side = new Vector3 (5, 0, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!gameManager.isFrozen) {
-			enemy.speed = speed;
-			enemy.SetDestination (player.transform.position + side);
-			Debug.Log ("Moving to player");
+			ChasePlayer();
+		}
+	}
+
+	void ChasePlayer(){
+		enemy.speed = speed;
+		if (chaseSide == AppearSide.APPEAR_LEFT) {
+			enemy.SetDestination (player.transform.position 
+			                      + 3*player.transform.forward - 5*player.transform.right);
+			Debug.Log ("Chasing left");
+		} else {
+			enemy.SetDestination (player.transform.position 
+			                      + 3*player.transform.forward + 5*player.transform.right);
+			Debug.Log ("Chasing right");
 		}
 	}
 }
