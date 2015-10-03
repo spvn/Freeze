e21dd16d294389c5fec30df4c48de78255689dfd
Matthew Tan <snowfall91@live.com>
2015-10-04 04:12:04 +0800

    Refactor player movement script
    Removed unused code

--- Assets/Script/script2ORMovement.cs
+++ Assets/Script/script2ORMovement.cs
@@ -5,53 +5,56 @@ using UnityStandardAssets.ImageEffects;
 //TODO: movement based on events
 
 public class script2ORMovement : MonoBehaviour {
-	
-	public float playerSpeed = 0.0f;
+
+    // Player movement variables
+    public float playerSpeed = 4.0f;
 	public float lateralSpeed = 5.0f;
-	public bool hasCollisionInFront;
-	public float collisionDist = 0.51f;
-	public float rotateSpeed = 2.0f;
-	public Transform[] path;
-	public GameObject canvas;
-	public GameManager gameManager;
-	
-	private CharacterController controller;
-	private Vector3 sideDirection;
-	private float playerWidth;
-
-	private int currNode = 0;
-	private Vector3 forwardDirection;
-	private ColorCorrectionCurves[] cccObjects;
-	private NoiseAndScratches[] nsObjects;
-	private Vector3 lastFramePos;
-	private Vector3 currFramePos;
-	Vector3 oculusMovement;
-    //private Animator playerAnimator;
-
+    public float rotateSpeed = 2.0f;
+    private Vector3 sideDirection;
+    private Vector3 forwardDirection;
     // For testing of jump
     public float jumpSpeed = 10.0f;
     public float gravity = 9.81f;
     private float verticalVelocity = 0f;
     private Vector3 moveDirection;
 
-    // Use this for initialization
+    // Essential GameObjects 
+    private GameManager gameManager;
+    private CharacterController controller;
+    private ColorCorrectionCurves[] cccObjects;
+    private NoiseAndScratches[] nsObjects;
+
+    //public bool hasCollisionInFront;
+    //public float collisionDist = 0.51f;
+
+    public Transform[] path;
+	public GameObject canvas;
+    private int currNode = 0;  
+
+    //private float playerWidth;
+	//private Vector3 lastFramePos;
+	//private Vector3 currFramePos;
+	//Vector3 oculusMovement;
+    //private Animator playerAnimator;
+
     void Start () {
 		controller = GetComponent<CharacterController>();
-		playerWidth = controller.radius * 2;
-		forwardDirection = Vector3.Normalize (path[0].position - transform.position);
-		gameManager = GameObject.Find ("Game Manager").GetComponent<GameManager>();
-		cccObjects = GetComponentsInChildren<ColorCorrectionCurves>();
-		nsObjects = GetComponentsInChildren<NoiseAndScratches>();
-		//playerAnimator = this.transform.Find ("playerAnimator").GetComponent<Animator> ();
-		
-		lastFramePos = transform.GetChild(1).transform.localPosition;
-	}
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        cccObjects = GetComponentsInChildren<ColorCorrectionCurves>();
+        nsObjects = GetComponentsInChildren<NoiseAndScratches>();
+
+        forwardDirection = Vector3.Normalize (path[0].position - transform.position);
+        
+        //lastFramePos = transform.GetChild(1).transform.localPosition;
+        //playerAnimator = this.transform.Find ("playerAnimator").GetComponent<Animator> ();
+        //playerWidth = controller.radius * 2;
+    }
 
 	// Update is called once per frame
 	void Update () {
 
         // Jump
-        if (controller.isGrounded && !gameManager.isGameOver)
+        if (controller.isGrounded && !gameManager.isFrozen)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
@@ -61,28 +64,38 @@ public class script2ORMovement : MonoBehaviour {
                 moveDirection.y = jumpSpeed;
             }
         }
-        // if player jumps
+        // Gravity
         moveDirection.y -= gravity * Time.deltaTime;
+        print(moveDirection.y);
         controller.Move(moveDirection * Time.deltaTime);
+
+        // Forward and lateral movement
+        if (!gameManager.isFrozen)
+        {
+            //Time.timeScale = 1.0f; FOR SLOWMO PURPOSES.
+            float horizontal = Input.GetAxis("Horizontal");
+            sideDirection = new Vector3(horizontal, 0, 0);
+            sideDirection = transform.rotation * sideDirection;
+            controller.SimpleMove((forwardDirection * playerSpeed) + (sideDirection * lateralSpeed));
+        }
 
         if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.JoystickButton5))) {
 			OVRManager.display.RecenterPose();  
 		} 
 
         // Get position of player's hit area
-		currFramePos = transform.GetChild(1).transform.localPosition;
+		//currFramePos = transform.GetChild(1).transform.localPosition;
 		
-
 		if( Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown (KeyCode.JoystickButton1)) {
 			meleeAttack();
 		}
 		
 		if (!gameManager.isGameOver && (Input.GetKeyDown (KeyCode.J) || Input.GetKeyDown (KeyCode.JoystickButton0))) {
 			//Debug.Log("Pressed Freeze " + isFrozen );
-			
 			StartCoroutine (glitchEffect ());
 		}
-
+        
+        /*
 		if (!gameManager.isFrozen) {
 			//Time.timeScale = 1.0f; FOR SLOWMO PURPOSES.
 			
@@ -102,13 +115,13 @@ public class script2ORMovement : MonoBehaviour {
 			}else {
 				controller.SimpleMove ((sideDirection * lateralSpeed));
 			}
-
+            
             //direction = Vector3.Normalize(direction);
             //Debug.Log ("AFTER: " + direction);
             //oculusMovement = lastFramePos - currFramePos;
 
             //Debug.Log ((direction * playerSpeed));
-
+            
 			RaycastHit hit;
 			Ray checkCollisionRayLeft = new Ray (transform.position + (-transform.right * (playerWidth / 2)) + transform.forward*playerWidth/2, transform.forward);
 			Ray checkCollisionRayRight = new Ray (transform.position + (transform.right * (playerWidth / 2)) + transform.forward*playerWidth/2, transform.forward);
@@ -122,6 +135,7 @@ public class script2ORMovement : MonoBehaviour {
             } else {
 				hasCollisionInFront = false;
 			}
+            
 		} 
 		
 		else {
@@ -132,15 +146,17 @@ public class script2ORMovement : MonoBehaviour {
 				obj.enabled = true;
 			}
 		}
-		lastFramePos = currFramePos;
+        */
+        //lastFramePos = currFramePos;
 	}
 
 	void OnTriggerEnter( Collider col )
 	{	
+        // if player collides with a node
 		if (col.gameObject.layer == 10) {
 			col.gameObject.SetActive(false);
 			//GameObject.Destroy(col.gameObject);
-			Debug.Log("working");
+			//Debug.Log("working");
 			currNode++;
 	
 			if (currNode == path.Length) {
@@ -155,26 +171,25 @@ public class script2ORMovement : MonoBehaviour {
 		}	
 	}
 
-	IEnumerator glitchEffect() {
-		
-		foreach (NoiseAndScratches obj in nsObjects) {
+	IEnumerator glitchEffect()
+    {
+		foreach (NoiseAndScratches obj in nsObjects)
+        {
 			obj.enabled = true;
 		}
-		
-		
 		yield return new WaitForSeconds(0.5f);
-		
-		foreach (NoiseAndScratches obj in nsObjects) {
+		foreach (NoiseAndScratches obj in nsObjects)
+        {
 			obj.enabled = false;
-		}
-		
+		}		
 	}
 
-	IEnumerator RotateTowards(Quaternion targetRotation) {
-		
+	IEnumerator RotateTowards(Quaternion targetRotation)
+    {
 		float t;
-		for (t = 0f; t<rotateSpeed; t+= Time.deltaTime ) {		
-			transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, t/rotateSpeed);
+		for (t = 0f; t < rotateSpeed; t += Time.deltaTime)
+        {		
+			transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, t / rotateSpeed);
 			yield return null;
 		}
 		
