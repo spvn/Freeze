20f3ffdd4d2a83f1b45b22f058f6419f0a2e3f34
abeltanjq <abeltanjq@gmail.com>
2015-10-18 11:57:43 +0800

    Added bgm to main menu and level 1

--- Assets/Script/scriptAudio.cs
+++ Assets/Script/scriptAudio.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class scriptAudio : MonoBehaviour {
 
 	public AudioClip[] freezeSounds; //0 is freeze, 1 is unfreeze
-	public AudioClip bgm;
+//	public AudioClip bgm;
 	
 	AudioSource audio;
 	
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
+		audio.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (audio.isPlaying == false) {
+	//	if (audio.isPlaying == false) {
 			//audio.clip = bgm;
 			//audio.Play ();
-		}
+	//	}
 	}
 	
 	public void playFreezeAudio() {
