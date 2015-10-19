47441b798331066f18698766823b4e5107b6ce1a
abeltanjq <abeltanjq@gmail.com>
2015-10-20 01:05:08 +0800

    Converted FadingScript to C#

---
+++ Assets/Script/Player/FadingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadingScript : MonoBehaviour {
+	
+	public float countdownTime = 10.0f;
+	Color fadeEffect;
+
+	void Start() {
+		fadeEffect = GetComponent<Renderer>().material.color;
+		fadeEffect.a = 1.0f;
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		if (countdownTime >= 0.0){
+			countdownTime -= Time.deltaTime;
+			fadeEffect.a  -=  0.3f * Time.deltaTime;
+			GetComponent<Renderer>().material.color = fadeEffect;
+		} else {
+			fadeEffect.a  = 0.0f;	
+			GetComponent<Renderer>().material.color = fadeEffect;
+			countdownTime = -1.0f;
+		}
+
+	}
+}
