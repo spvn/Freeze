33c1979f43a6abf8a7acc700ef4f59159a93552f
Matthew Tan <snowfall91@live.com>
2015-10-22 01:49:51 +0800

    Add jump to player movement (boss)

--- Assets/Script/Player/Player_BossMovement.cs
+++ Assets/Script/Player/Player_BossMovement.cs
@@ -5,14 +5,22 @@ using UnityStandardAssets.ImageEffects;
 public class Player_BossMovement : MonoBehaviour {
 
     public float rotationSpeed = 1.0f;
+    public float jumpSpeed = 10f;
+    public float gravity = 1.81f;
     public GameObject focalPoint;
     public LevelManager levelManager;
+
     private NoiseAndScratches[] nsObjects;
+    private CharacterController controller;
+    private Vector3 downDirection = Vector3.zero;
+    private float continueRotation;
 
     void Start () {
+        continueRotation = rotationSpeed; // to be used while in the air
         nsObjects = GetComponentsInChildren<NoiseAndScratches>();
+        controller = GetComponent<CharacterController>();
     }
-	
+
 	void Update () {
         /*
         if (levelManager.isGameOver || levelManager.isFrozen || levelManager.isPause)
@@ -31,19 +39,25 @@ public class Player_BossMovement : MonoBehaviour {
             attack.deflectBossProjectile();
         }
 
-	    if ( Input.GetKey(KeyCode.A) )
+	    if ( Input.GetKey(KeyCode.A) && controller.isGrounded )
         {
+            continueRotation = rotationSpeed; // will be used to continue rotation in the air if player jumps
             focalPoint.transform.Rotate(new Vector3(0, rotationSpeed, 0));
         }
-        else if ( Input.GetKey(KeyCode.D) )
+        else if ( Input.GetKey(KeyCode.D) && controller.isGrounded )
         {
+            continueRotation = -rotationSpeed; // will be used to continue rotation in the air if player jumps
             focalPoint.transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        } else if (controller.isGrounded)
+        {
+            continueRotation = 0; // no rotation by default if player jumps without moving
         }
 
-        if ( Input.GetKey(KeyCode.LeftAlt) )
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            jump();
+            downDirection = new Vector3(0, jumpSpeed, 0);
         }
+        updateFrameMovement();
     }
 
     IEnumerator glitchEffect()
@@ -61,6 +75,17 @@ public class Player_BossMovement : MonoBehaviour {
 
     void jump()
     {
+        downDirection = new Vector3(0, jumpSpeed, 0);
+    }
 
+    void updateFrameMovement()
+    {
+        downDirection.y -= gravity * Time.deltaTime;
+        controller.Move(downDirection * Time.deltaTime);
+        // Player in air will continue moving
+        if (!controller.isGrounded)
+        {
+            focalPoint.transform.Rotate(new Vector3(0, continueRotation, 0));
+        }
     }
 }
