8a777db5e951723f4ce6c69247540bb53b392f99
Matthew Tan <snowfall91@live.com>
2015-10-21 20:05:06 +0800

    Fix pulse visual not appearing in lvl 2 onwards. Add nodes for lvl 3

--- Assets/Script/Player/PulseAttack.cs
+++ Assets/Script/Player/PulseAttack.cs
@@ -34,7 +34,7 @@ public class PulseAttack : MonoBehaviour {
         // Depleting action bar stock
         if ( actionBar.canDeplete(energyCost) )
         {
-            Instantiate(pulseEffect, transform.position - new Vector3(0,transform.position.y,0), transform.rotation);
+            Instantiate(pulseEffect, transform.position - new Vector3(0,1.5f,0), transform.rotation);
             actionBar.DepleteActionStock(energyCost);
 
             Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, pulseRadius);
