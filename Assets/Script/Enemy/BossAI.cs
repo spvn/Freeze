339f303c45ce5214c02eb630a8b955fd43c962cf
pigoblock <kowanling@hotmail.com>
2015-10-26 21:36:00 +0800

    BossHealth BossAI stub

---
+++ Assets/Script/Enemy/BossAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossAI : MonoBehaviour {
+
+	private GameObject player;
+	private LevelManager levelManager;
+
+	private bool bossActivated;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("OVRCameraRig");
+		levelManager = GameObject.Find ("Level Manager").GetComponent<LevelManager>();
+
+		bossActivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!levelManager.isFrozen && !levelManager.isPause) {
+			if (bossActivated){
+				LookAtPlayer();
+			}
+		}
+	}
+
+	private void LookAtPlayer(){
+		transform.LookAt(player.transform.position);
+	}
+
+	public void ActivateBoss(){
+		bossActivated = true;
+	}
+}
---
+++ Assets/Script/Enemy/BossHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossHealth : MonoBehaviour {
+
+	private GameObject player;
+	private LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("OVRCameraRig");
+		levelManager = GameObject.Find ("Level Manager").GetComponent<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
