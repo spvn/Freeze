2c181ebb0087c8db9f1106dcae9b85047fde25d5
Matthew Tan <snowfall91@live.com>
2015-10-28 00:52:13 +0800

    Level 1 - add window shattering animation

---
+++ Assets/Script/Environment/ShatterTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShatterTrigger : MonoBehaviour {
+
+    public GameObject window;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider col)
+    {
+        print("Shattering");
+        if (col.gameObject.layer == 9)
+        {
+            window.SetActive(true);
+        }
+    }
+}
