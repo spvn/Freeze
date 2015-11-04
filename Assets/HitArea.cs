e78c0393d02e84ea53b7c5d9edac637696000134
spvn <wkan2003@gmail.com>
2015-11-04 20:29:54 +0800

    Changed oculus hit detection

---
+++ Assets/HitArea.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void LateUpdate () {
+		transform.rotation = Quaternion.identity;
+	}
+}
