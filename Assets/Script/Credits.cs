80e46d723c834d1391e6f184213ff45a37acecc2
abeltanjq <abeltanjq@gmail.com>
2015-11-09 02:17:13 +0800

    Added Credits Scene

---
+++ Assets/Script/Credits.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) /* || Input.GetKeyDown(KeyCode.JoystickButton0)*/){
+			Application.LoadLevel(0);
+		} 
+	}
+}
