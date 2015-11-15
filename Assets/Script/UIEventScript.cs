7058abf23c8fa3fd4a5169823bfee8d5c94f1500
abeltanjq <abeltanjq@gmail.com>
2015-11-16 01:50:23 +0800

    Changed tutorial text to appear xbox controls Added a control page

--- Assets/Script/UIEventScript.cs
+++ Assets/Script/UIEventScript.cs
@@ -23,17 +23,17 @@ public class UIEventScript : MonoBehaviour {
 			{
 			case "jump":
 				Debug.Log("Entered");
-				canvasEventText.GetComponent<Text>().text = "JUMP: L-ALT";
+				canvasEventText.GetComponent<Text>().text = "JUMP: X";
 				break;
 			case "move":
 				Debug.Log("Moved");
 				canvasEventText.GetComponent<Text>().text = "MOVE YOUR HEAD";
 				break;
 			case "pulse":
-				canvasEventText.GetComponent<Text>().text = "PULSE: K";
+				canvasEventText.GetComponent<Text>().text = "PULSE: B";
 				break;
 			case "strafe":
-				canvasEventText.GetComponent<Text>().text = "STRAFE: A/D";
+				canvasEventText.GetComponent<Text>().text = "STRAFE: LEFT/RIGHT AXIS";
 				break;
 			case "deflect":
 				canvasEventText.GetComponent<Text>().text = "USE PULSE TO DEFLECT ROCKETS";
