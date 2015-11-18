86dd8c105fc0adf9c4066399ba0e860ad3e991b2
spvn <wkan2003@gmail.com>
2015-11-18 16:12:04 +0800

    A lot of game balancing

--- Assets/Script/Player/Underwater.cs
+++ Assets/Script/Player/Underwater.cs
@@ -10,31 +10,39 @@ public class Underwater : MonoBehaviour {
 	public GameObject centerEyeAnchor;
 	
 	//The scene's default fog settings
-	private bool defaultFog = RenderSettings.fog;
-	private Color defaultFogColor = RenderSettings.fogColor;
-	private float defaultFogDensity = RenderSettings.fogDensity;
-	private Material defaultSkybox = RenderSettings.skybox;
+	private bool defaultFog;
+	private Color defaultFogColor;
+	private float defaultFogDensity;
 	private Material noSkybox;
+	private Material defaultSkybox;
 	
 	void Start () {
 		//Set the background color
+		defaultFog = RenderSettings.fog;
+		defaultFogColor = RenderSettings.fogColor;
+		defaultFogDensity = RenderSettings.fogDensity;
+		defaultSkybox = RenderSettings.skybox;
+
 		centerEyeAnchor.GetComponent<Camera>().backgroundColor = new Color(0, 0.4f, 0.7f, 1);
 	}
 	
 	void Update () {
 		if (centerEyeAnchor.transform.position.y < underwaterLevel)
 		{
+			Debug.Log ("BELOW WATER LEVEL");
 			RenderSettings.fog = true;
 			RenderSettings.fogColor = new Color(0, 0.4f, 0.7f, 0.6f);
 			RenderSettings.fogDensity = 0.04f;
 			RenderSettings.skybox = noSkybox;
 		}
-		/*else
+		else
 		{
 			RenderSettings.fog = defaultFog;
 			RenderSettings.fogColor = defaultFogColor;
 			RenderSettings.fogDensity = defaultFogDensity;
 			RenderSettings.skybox = defaultSkybox;
-		}*/
+		}
 	}
+
+
 }
